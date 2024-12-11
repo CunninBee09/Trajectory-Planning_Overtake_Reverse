e0db1f193a8b27f1f465ba6c45a043deb87c1429
CunninBee09 <sat0901@outlook.com>
2024-12-11 20:09:58 +0100

    seperated all the func into respective files

--- Overtake/ego_vehicle.py
+++ Overtake/ego_vehicle.py
@@ -9,39 +9,9 @@ sys.path.append(str(pathlib.Path(__file__).parent.parent))
 
 from FrenetOptimalTrajectory.frenet_optimal_trajectory import \
     frenet_optimal_planning
+from utilis import *
 
-from CubicSplinePlanner import cubic_spline_planner
-from matplotlib.patches import Rectangle
-
-road_width = 10  #meters
-obs_length = 5   #meters
-obs_width = 2    #meters
-SIM_LOOP = 1000
-MAX_EGO_SPEED = 60.0 / 3.6  # maximum speed of Ego vehicle [m/s]
-MAX_EGO_ACCEL = 2.0  # maximum acceleration of Ego vehicle [m/ss]
-MAX_OBS_SPEED = 15.0 / 3.6  # maximum speed of Obstacle [m/s]
-MAX_OBS_ACCEL = 10.0  # maximum acceleration of Obstacle [m/ss]
-MAX_CURVATURE = 10.0  # maximum curvature [1/m]
-MAX_ROAD_WIDTH = 3.0  # maximum road width [m]
-D_ROAD_W = 1 # road width sampling length [m]
-DT = 0.2  # time tick [s]
-MAX_T = 4.4  # max prediction time [m]
-MIN_T = 4.0  # min prediction time [m]
-TARGET_SPEED = 30.0 / 3.6  # target speed [m/s]
-D_T_S = 5.0 / 3.6  # target speed sampling length [m/s]
-N_S_SAMPLE = 4 # sampling number of target speed
-ego_length = 5 #car_length [m]
-ego_width = 2 #car_width [m]
-ROBOT_RADIUS = 5.0# robot radius [m]
-
-# cost weights
-K_J = 0.1
-K_T = 0.1
-K_D = 1.5      
-K_LAT = 1
-K_LON = 0.5
-
-show_animation = True
+# from CubicSplinePlanner import cubic_spline_planner
 
 class QuarticPolynomial:
 
@@ -82,7 +52,6 @@ class QuarticPolynomial:
         xt = 6 * self.a3 + 24 * self.a4 * t
 
         return xt
-
 
 class QuinticPolynomial:
 
@@ -127,7 +96,6 @@ class QuinticPolynomial:
 
         return xt
 
-    
 class FrenetPath:
 
     def __init__(self):
@@ -149,8 +117,7 @@ class FrenetPath:
         self.yaw = []
         self.ds = []
         self.c = []
-
-
+        
 def calc_frenet_paths(c_speed, c_accel, c_d, c_d_d, c_d_dd, s0, V):
     frenet_paths = []
 
@@ -194,7 +161,7 @@ def calc_frenet_paths(c_speed, c_accel, c_d, c_d_d, c_d_dd, s0, V):
                 frenet_paths.append(tfp)
 
     return frenet_paths
-
+    # pass  # Keep the implementation from your original code
 
 def calc_global_paths(fplist, csp):
     for fp in fplist:
@@ -233,9 +200,9 @@ def calc_global_paths(fplist, csp):
 
 
     return fplist
+    # pass  # Keep the implementation from your original code
 
-
-def check_collision(fp, obs_path ):
+def check_collision(fp, obs_path):
     d = []
     for i in range(min(len(fp.x), len(obs_path.x))):
         dx = ((fp.x[i] - obs_path.x[i]) ** 2)
@@ -248,7 +215,7 @@ def check_collision(fp, obs_path ):
         return False
 
     return True
-
+    # pass  # Keep the implementation from your original code
 
 def check_paths(fplist, obs_path):
     ok_ind = []
@@ -277,26 +244,7 @@ def check_paths(fplist, obs_path):
               print("All candidate paths failed constraints!")  
 
         return [fplist[i] for i in ok_ind]
-
-    
-def check_obs_paths(obslist):
-    ok_ind = []
-    for i, _ in enumerate(obslist):
-        if any([v > MAX_OBS_SPEED for v in obslist[i].s_d]):  # Max speed check
-            continue
-        elif any([abs(a) > MAX_OBS_ACCEL for a in
-                  obslist[i].s_dd]):  # Max accel check
-            continue
-        elif any([abs(c) > MAX_CURVATURE for c in
-                  obslist[i].c]):  # Max curvature check
-            continue
-        # elif not check_collision(fplist[i], ob):
-        #     continue
-
-        ok_ind.append(i)
-
-    return [obslist[i] for i in ok_ind]
-
+    # pass  # Keep the implementation from your original code
 
 def frenet_optimal_planning(csp, s0, c_speed, c_accel, c_d, c_d_d, c_d_dd, obs_path, V):
     fplist = calc_frenet_paths(c_speed, c_accel, c_d, c_d_d, c_d_dd, s0, V)
@@ -324,22 +272,7 @@ def frenet_optimal_planning(csp, s0, c_speed, c_accel, c_d, c_d_d, c_d_dd, obs_p
     # sorted_fplist = fplist.sort(key=lambda x: x.cf)
     
     return best_path, fplist
-
-
-def obstacle_planning(csp, obs_s0, obs_speed, obs_acc, obs_d, obs_d_d, obs_d_dd, V= 15/3.6):
-    obslist= calc_frenet_paths(obs_speed, obs_acc, obs_d, obs_d_d, obs_d_dd, obs_s0, V)
-    obslist= calc_global_paths(obslist, csp)
-    obslist= check_obs_paths(obslist)
-    
-    # find minimum cost path
-    min_cost = float("inf")
-    best_obs_path = None
-    for ob in obslist:
-        if min_cost >= ob.cf:
-            min_cost = ob.cf
-            best_obs_path = ob
-
-    return best_obs_path
+    # pass  # Keep the implementation from your original code
 
 def parameter(csp, s0, c_speed, c_accel, c_d, c_d_d, c_d_dd, obs_path):
     if obs_path.x[0] - s0 > 20:
@@ -352,160 +285,4 @@ def parameter(csp, s0, c_speed, c_accel, c_d, c_d_d, c_d_dd, obs_path):
         V3 = 30/3.6
         path, fplist= frenet_optimal_planning(csp, s0, c_speed, c_accel, c_d, c_d_d, c_d_dd, obs_path, V3)
     return path, fplist
-
-def generate_target_course(x, y):
-    csp = cubic_spline_planner.CubicSpline2D(x, y)
-    s = np.arange(0, csp.s[-1], 0.1)
-
-    rx, ry, ryaw, rk = [], [], [], []
-    for i_s in s:
-        ix, iy = csp.calc_position(i_s)
-        rx.append(ix)
-        ry.append(iy)
-        ryaw.append(csp.calc_yaw(i_s))
-        rk.append(csp.calc_curvature(i_s))
-
-    return rx, ry, ryaw, rk, csp
-
-    
-def main():
-    print(__file__ + " start!!")
-    
-    wx = [0.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0, 110.0, 120.0, 130.0, 140.0, 150.0]
-    wy = [0.0, 10.0, 10.0, 5.0, 0.0, -5.0, -5.0, 0.0, 2.0, 5.0, 7.0, 10.0, 5.0, 3.0, -1.0, -4.0]
-    # wx = [0.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0, 110.0, 120.0, 130.0, 140.0, 150.0]
-    # wy = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    tx, ty, tyaw, tc, csp = generate_target_course(wx, wy)
-    
-    area = 25.0
-    
-    csp.ux = [x + 7.5*math.cos(i_yaw + math.pi / 2.0) for x,i_yaw in zip(tx,tyaw) ]
-    csp.uy = [y + 7.5*math.sin(i_yaw + math.pi / 2.0) for y,i_yaw in zip(ty,tyaw) ]
-    csp.lx = [x - 2.5*math.cos(i_yaw + math.pi / 2.0) for x,i_yaw in zip(tx,tyaw) ]
-    csp.ly = [y - 2.5*math.sin(i_yaw + math.pi / 2.0) for y,i_yaw in zip(ty,tyaw) ]
-    csp.mx = [x + 2.5*math.cos(i_yaw + math.pi / 2.0) for x,i_yaw in zip(tx,tyaw) ]
-    csp.my = [y + 2.5*math.sin(i_yaw + math.pi / 2.0) for y,i_yaw in zip(ty,tyaw) ]
-    
- # initial state of obs vehicle
-    obs_s0 = 40.0 # current position
-    obs_speed = 15.0 / 3.6  # current speed [m/s]
-    obs_acc= 0.0  # current acceleration [m/ss]
-    obs_d = 0.0 #lateral positon[m]
-    obs_d_d = 0.0 #lateral velocity[m/s]
-    obs_d_dd = 0.0 #lateral acceleration[m/ss]
-    
- # initial state of ego vehicle
-    c_speed = 30.0 / 3.6  # current speed [m/s]
-    c_accel = 0.0  # current acceleration [m/ss]
-    c_d = 0.0  # current lateral position [m]
-    c_d_d = 0.0  # current lateral speed [m/s]
-    c_d_dd = 0.0  # current lateral acceleration [m/s]
-    s0 = 0.0  # current course position
-    
-    for i in range(SIM_LOOP):
-        
-        obs_path = obstacle_planning(csp, obs_s0, obs_speed, obs_acc, obs_d, obs_d_d, obs_d_dd)
-        path,fplist = parameter(csp, s0, c_speed, c_accel, c_d, c_d_d, c_d_dd, obs_path)
-        # path, fplist = frenet_optimal_planning(csp, s0, c_speed, c_accel, c_d, c_d_d, c_d_dd, obs_path, csp.uy ,csp.ly)
-        
-        
-        if path is None:
-            break
-        
-        if np.hypot(path.x[1] - tx[-1], path.y[1] - ty[-1]) <= 0.0:
-            print("Goal")
-            break
-        
-        obs_s0 = obs_path.s[1]
-        obs_d = obs_path.d[1]
-        obs_d_d = obs_path.d_d[1]
-        obs_d_dd = obs_path.d_dd[1]
-        obs_speed = obs_path.s_d[1]
-        obs_acc = obs_path.s_dd[1]
-        
-        # path = frenet_optimal_planning(csp, s0, c_speed, c_accel, c_d, c_d_d, c_d_dd, obs_path, uy)
-        # if path is None:
-        #     print("No valid path found for ego vehicle!")
-        #     break
-        
-        s0 = path.s[1]
-        c_d = path.d[1]
-        c_d_d = path.d_d[1]
-        c_d_dd = path.d_dd[1]
-        c_speed = path.s_d[1]
-        c_accel = path.s_dd[1]
-        
-        # plt.figure()
-        # for i, path in enumerate(fplist[:10]):
-        #   plt.plot(path.x, path.y, label=f"Path {i} (Cost: {path.cf:.2f})")
-
-
-        if show_animation:  # pragma: no cover
-            plt.cla()
-            # for stopping simulation with the esc key.
-            plt.gcf().canvas.mpl_connect(
-                'key_release_event',
-                lambda event: [exit(0) if event.key == 'escape' else None])
-             
-            #Draw ego vehicle as rectangle
-            ego_yaw = path.yaw[1] * 180 / np.pi  # Convert from radians to degrees for matplotlib
-            ego_vehicle = Rectangle(
-             (path.x[1] - ego_length / 2, path.y[1] - ego_width / 2),  # Bottom-left corner
-              ego_length,  # Length
-              ego_width,   # Width
-            #   angle = ego_yaw,
-              edgecolor="blue",
-              facecolor="none"
-            )
-            
-            # Apply rotation around the center of the rectangle
-            t = transforms.Affine2D().rotate_deg_around(path.x[1], path.y[1], ego_yaw) + plt.gca().transData
-            ego_vehicle.set_transform(t)
-
-            plt.gca().add_patch(ego_vehicle)
-            
-            # Plot the top 10 paths
-            for i, fp in enumerate(fplist[:10]):  # Only plot the top 10 paths
-               plt.plot(fp.x, fp.y, label=f"Path {i + 1} (Cost: {fp.cf:.4f})", linestyle="--")
-               
-            # Draw obstacle vehicle as a rectangle
-            obs_yaw = obs_path.yaw[1] * 180 / np.pi
-            obs_vehicle = Rectangle(
-             (obs_path.x[1] - obs_length / 2, obs_path.y[1] - obs_width / 2),  # Bottom-left corner
-              obs_length,  # Length
-              obs_width,   # Width
-              edgecolor="red",
-              facecolor="none"
-            )
-            t = transforms.Affine2D().rotate_deg_around(obs_path.x[1], obs_path.y[1], obs_yaw) + plt.gca().transData
-            obs_vehicle.set_transform(t)
-            
-            plt.gca().add_patch(obs_vehicle)
-            
-            
-            plt.plot(tx, ty)
-            plt.plot(csp.ux, csp.uy, '-k')
-            plt.plot(csp.lx, csp.ly, '-k')
-            plt.plot(csp.mx, csp.my, '--k')
-            plt.plot(obs_path.x[1:], obs_path.y[1:], "-r")
-            plt.plot(obs_path.x[1], obs_path.y[1], "vc")
-            plt.xlim(obs_path.x[1] - area, obs_path.x[1] + area)
-            plt.ylim(obs_path.y[1] - area, obs_path.y[1] + area)
-            plt.plot(path.x[1:], path.y[1:], "-r")
-            plt.plot(path.x[1], path.y[1], "vc")
-            plt.xlim(path.x[1] - area, path.x[1] + area)
-            plt.ylim(path.y[1] - area, path.y[1] + area)
-            plt.title(f"Ego v[km/h]: {c_speed * 3.6:.2f}, Obs v[km/h]: {obs_speed * 3.6:.2f}")
-            plt.grid(True)
-            plt.pause(0.0001)
-
-
-    print("Finish")
-    if show_animation:  # pragma: no cover
-        plt.grid(True)
-        plt.pause(0.0001)
-        plt.show()
-
-
-if __name__ == '__main__':
-    main()
+    # pass  # Keep the implementation from your original code
